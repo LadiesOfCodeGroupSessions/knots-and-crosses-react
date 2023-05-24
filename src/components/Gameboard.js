c0468d017821bdcbf785ecd0446687d64b4d3fd2
Karen Collins <fisheskc@googlemail.com>
2023-05-24 19:00:24 +0100

    adding a button component

---
+++ src/components/Gameboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Gameboard = (props) => {
+
+    const Square = (props) => {
+        return  <button>-</button>
+    }
+   
+  return (
+    
+    <div>Gameboard</div>
+  )
+}
