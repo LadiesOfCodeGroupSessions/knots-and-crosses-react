9139d34503010007fb6f72ebeaa24a07b91c6792
Karen Collins <fisheskc@googlemail.com>
2023-06-21 19:48:33 +0100

    This is the Gameboard component in React

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,13 +1,40 @@
 import React from 'react'
 
-export const Gameboard = (props) => {
+// note here need to be able to select the spaces. The css needs setting/connecting for this
 
+export const Gameboard = () => {
+  const [gameBoard, setGameBoard] = useState(["-", "-", "-", "-", "-", "-", "-", "-", "-"]);
+  const [score, setScore] = useState("");
+  const [winningPatterns, setWinningPatterns] = useState([
+    [0, 4, 8],
+    [0, 1, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+  ]);
+
+  function toggle(id) {
+  setGameBoard((prevGameBoard) => {
+    return prevGameBoard.map((gameBoard) => {
+      return gameBoard.id === id ? {...gameBoard, on: !gameBoard.on} : gameBoard
+    })
+  })
+}
     const Square = (props) => {
         return  <button>-</button>
     }
    
   return (
     
-    <div>Gameboard</div>
+    <div>
+    {
+
+    }
+    
+    
+    Gameboard</div>
   )
 }
---
+++ src/components/players.js
@@ -0,0 +1,7 @@
+// const [playerToken, setPlayerToken] = useState([{player1Token: "", player2Token: ""}]);
+  
+//   let findplayers = Object.keys(playerToken);
+//   let result = playerToken.filter((player) => {
+//     return findplayers.includes(player.player1Token) || findplayers.includes((player.player2Token))
+//   })
+//   return result
